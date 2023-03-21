efca01c859d1475fcb0ed3c5cba9c11b912a862b
boyka2708 <78980334+boyka2708@users.noreply.github.com>
2023-03-21 15:21:15 +0530

    Add files via upload

---
+++ capstone-main/capstone-main/src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+    <header>
+        <meta name="description" content="your text goes here"/>
+        <meta name="og:title" content=""/>
+        <meta name="og:description" content=""/>
+        <meta name="og:image" content=""/>
+    </header>
+    )
+}
+
+export default Header
---
+++ capstone-main/capstone-main/src/components/Nav.js
@@ -0,0 +1,12 @@
+function Nav() {
+    return (
+        <nav>
+            <img src='' alt='Little Lemon Logo' />
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
